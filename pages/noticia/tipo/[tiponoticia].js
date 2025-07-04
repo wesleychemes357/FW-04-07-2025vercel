9a3bab32d57fcacf3123ff3f463b7a60d2310324
Ronaldo <150154326+wesleychemes357@users.noreply.github.com>
2025-07-04 08:56:46 -0300

    Add files via upload

---
+++ pages/noticia/tipo/[tiponoticia].js
@@ -0,0 +1,26 @@
+import { Container, Row } from "react-bootstrap";
+import Cards from "../../components/cards";
+export async function getServerSideProps(req) {
+  // Fetch data from  API notícias
+  const res = await fetch('http://localhost:3000/api/noticias/tipo/' + req.query.tiponoticia)
+  const repo = await res.json()
+  // Pass data to the page via props
+  return { props: { noticias: repo } }
+}
+export default function Page({ noticias }) {
+  return <>
+    <Container>
+      <Row xs={1} md={3}>
+        {Array.isArray(noticias) ?
+          noticias.map(noticia => (
+            <Cards idnoticia={noticia.idnoticia}
+              conteudonoticia={noticia.conteudonoticia}
+              titulonoticia={noticia.titulonoticia}
+              datahoracadastro={noticia.datahoracadastro}
+              tiponoticia={noticia.tiponoticia} />
+          ))
+          : "não"}
+      </Row>
+    </Container>
+  </>
+}
